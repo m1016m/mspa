611cbc88228b4b84109c25d72d4d2cc771bcb59c
m1016m <m1016m@gmail.com>
2023-08-12 21:58:26 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://m1016m:3SEOJjK0BODmVoultGEnkr4ggM7pLi16@dpg-cja6902683bs739mocc0-a.singapore-postgres.render.com/mspa'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://m1016m:aynpduYUgaD2yAVFd8JLBgj2TPMPSWGD@dpg-cjbor97db61s73aeaou0-a.singapore-postgres.render.com/mspa_d63k'
 
 
 class ProdConfig(Config):
